b4147e048e60c10d8000ca4146a9a1dc57a847e1
Xyzkrooo <mhmdfarasaufaa@gmail.com>
2024-05-21 15:10:13 +0700

    push kedua

---
+++ app/Models/brand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class brand extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['id', 'nama_brand'];
+    public $timestamps = true;
+}
